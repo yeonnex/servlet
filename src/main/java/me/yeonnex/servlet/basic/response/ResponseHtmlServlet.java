7247905740992de2ad91cbba32f07c44e894fe84
yeonnex <yeonnex@gmail.com>
2022-12-31 00:32:10 +0900

    Http 응답데이터 - 단순 텍스트, HTML

---
+++ src/main/java/me/yeonnex/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,25 @@
+package me.yeonnex.servlet.basic.response;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+// Html 리
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)throws IOException {
+        // Content-Type: text/html;charset=utf-8;
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("    <div>안녕</div>");
+        writer.println("<body/>");
+    }
+}
